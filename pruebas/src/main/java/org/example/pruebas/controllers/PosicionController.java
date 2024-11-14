dea860bd92f5ae5431483711a3473f1177ee523f
JUAN <juanichiapero@gmail.com>
2024-11-14 16:10:58 -0300

    Controller de posicion terminado

--- pruebas/src/main/java/org/example/pruebas/controllers/PosicionController.java
+++ pruebas/src/main/java/org/example/pruebas/controllers/PosicionController.java
@@ -11,6 +11,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Controlador REST para gestionar operaciones relacionadas con la posición.
+ * Mapea las solicitudes a "/api/pruebas".
+ */
+
 @RestController
 @RequestMapping("/api/pruebas")
 public class PosicionController {
@@ -21,17 +26,22 @@ public class PosicionController {
         this.posicionService = posicionService;
     }
 
-    // 4 - Endpoint para guardar una posicion
+    /**
+     * Endpoint para guardar una nueva posición en el sistema.
+     * Recibe un objeto PosicionDTO (que contiene la información de la posición a guardar) en el cuerpo de la solicitud.
+     * retorna una respuesta HTTP con el estado de la operación
+     */
     @PostMapping("/position")
     public ResponseEntity<String> guardarPosicion(@RequestBody PosicionDTO posicionDTO) {
         try {
-            // Llama al servicio para crear una nueva posición
+            // Llama al servicio para crear una nueva instancia de posición con los datos del DTO.
             Posicion posicion = posicionService.crearNuevaPosicion(
                     posicionDTO.getId_vehiculo(),
                     posicionDTO.getLongitud(),
                     posicionDTO.getLatitud()
             );
             return ResponseEntity.ok("Posición guardada");
+            // Manejo de excepciones: imprime el error y responde con error 500 en caso de fallo
         } catch (Exception e) {
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
